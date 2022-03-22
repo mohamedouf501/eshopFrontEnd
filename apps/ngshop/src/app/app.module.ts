8187131a26f1ae5c86083fb3f3ce72b73a6c9f1c
mohamedouf504 <mohamedouf504@gmail.com>
2022-03-22 03:08:48 +0200

    setup structure files

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,13 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductListComponent } from './product-list/product-list.component';
+const routes :Routes =[
+  {
+    path: '', component: HomePageComponent
+  },
+  {
+    path: 'products', component: ProductListComponent
+  }
+]
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
